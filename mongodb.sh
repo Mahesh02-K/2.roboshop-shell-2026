651401f4179782d2c7a19cc1138b7914880ec7bb
Mahesh02-K <kakuturumahesh@gmail.com>
2026-05-12 02:56:08 +0530

    Calling script from one to other

--- mongodb.sh
+++ mongodb.sh
@@ -3,20 +3,20 @@
 source ./common.sh
 root_verification
 
-cp mongo.repo /etc/yum.repos.d/mongodb.repo &>>$LOG_FILE
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 VERIFY $? "Copying mongo repo file"
 
 dnf install mongodb-org -y &>>$LOG_FILE
-VERIFY $? "Installing Mongodb server"
+VERIFY $? "Installing MongoDB"
 
 systemctl enable mongod &>>$LOG_FILE
 systemctl start mongod &>>$LOG_FILE
-VERIFY $? "Starting Mongodb"
+VERIFY $? "Starting MongoDB"
 
 sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf
 VERIFY $? "Editing Mongod config file to enable remote connections"
 
 systemctl restart mongod &>>$LOG_FILE
-VERIFY $? "Restarting MongoDB"
+VERIFY $? "Restarting Mongodb"
 
 print_time
