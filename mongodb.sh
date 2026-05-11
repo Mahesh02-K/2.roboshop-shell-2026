81315a47578fd0e1653b0cfbe497c7a7d2409e48
Mahesh02-K <kakuturumahesh@gmail.com>
2026-05-11 23:16:21 +0530

    Calling script from one to other

--- mongodb.sh
+++ mongodb.sh
@@ -3,7 +3,7 @@
 source ./common.sh
 root_verification
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 VERIFY $? "Copying mongo repo file"
 
 dnf install mongodb-org -y &>>$LOG_FILE
