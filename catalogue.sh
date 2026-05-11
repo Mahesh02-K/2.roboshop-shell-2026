1255fb30f3d8f81a13f65e521f7ef8e65c222bf2
Mahesh02-K <kakuturumahesh@gmail.com>
2026-05-12 02:00:22 +0530

    Calling script from one to other

--- catalogue.sh
+++ catalogue.sh
@@ -8,17 +8,17 @@ app_setup
 nodejs_setup
 systemd_setup
 
-# cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-# dnf install mongodb-mongosh -y &>>$LOG_FILE
-# VERIFY $? "Installing Mongodb client"
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VERIFY $? "Installing Mongodb client"
 
-# STATUS=$(mongosh --host mongodb.kakuturu.online --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
-# if [ $STATUS -lt 0 ]
-# then
-#     mongosh --host mongodb.kakuturu.online </app/db/master-data.js &>>$LOG_FILE
-#     VERIFY $? "Loading data into MongoDB"
-# else
-#     echo -e "Data is already loaded ... $Y SKIPPING $N" | tee -a $LOG_FILE
-# fi .
+STATUS=$(mongosh --host mongodb.kakuturu.online --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $STATUS -lt 0 ]
+then
+    mongosh --host mongodb.kakuturu.online </app/db/master-data.js &>>$LOG_FILE
+    VERIFY $? "Loading data into MongoDB"
+else
+    echo -e "Data is already loaded ... $Y SKIPPING $N" | tee -a $LOG_FILE
+fi 
 
 print_time
